c9eedf628b6f5b89d6e64d8b645aa1fbb46769c6
UtsavJ <utsavjaiswal2004@gmail.com>
2025-04-09 23:15:34 +0530

    local storage and tasks services

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,6 +1,7 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { newTaskData } from '../tasks/task/task.model';
+import { newTaskData } from '../task/task.model';
+import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-new-task',
@@ -10,24 +11,25 @@ import { newTaskData } from '../tasks/task/task.model';
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
-  @Output() cancel = new EventEmitter<void>();
-  @Output() addNewTask = new EventEmitter<newTaskData>();
+  @Input({required:true}) userId!:string;
+  @Output() close = new EventEmitter<void>();
   enteredTitle = "";
   enteredSummary = "";
   enteredDueDate = "";
-  
+  private tasksService = inject(TasksService);
   onCancel(){
-    this.cancel.emit();
+    this.close.emit();
   }
 
   onSubmit(){
-    this.addNewTask.emit(
+    this.tasksService.addTask(
       {
         title:this.enteredTitle,
         summary:this.enteredSummary,
         dueDate:this.enteredDueDate
-      }
-    )
-    console.log('Form submitted');
+      },
+      this.userId
+    );
+    this.close.emit();
   }
 }
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,8 +1,9 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
 import { dummyTasks } from '../dummy_tasks';
-import { NewTaskComponent } from "../new-task/new-task.component";
+import { NewTaskComponent } from "./new-task/new-task.component";
 import { newTaskData} from './task/task.model';
+import {TasksService} from './tasks.service'
 
 @Component({
   selector: 'app-tasks',
@@ -13,38 +14,22 @@ import { newTaskData} from './task/task.model';
 })
 
 export class TasksComponent {
-  tasks = dummyTasks;
+  constructor(private tasksService:TasksService){}
   @Input({required: true}) name!:string;
   @Input({required:true}) userId!:string;
   isAddingNewTask: boolean = false;
 
   get selectedUsersTasks(){
-    return this.tasks.filter( (task)=> task.userId === this.userId)
+    return this.tasksService.getUserTasks(this.userId);
   }
-
-  onCompleteTask(id:string){
-    this.tasks = this.tasks.filter( (task)=> task.id!== id );
-    console.log("Task " + id + " has been completed");
-  }
-
+  
   onStartAddTask(){
     this.isAddingNewTask = true;
   }
 
-  onCancelAddTask(){
+  onCloseAddTask(){
     this.isAddingNewTask=false;
   }
 
-  onAddTask(newTask:newTaskData){
-    this.tasks.unshift({
-      id: new Date().getTime().toString(),
-      userId: this.userId,
-      title:newTask.title,
-      summary:newTask.summary,
-      dueDate:newTask.dueDate,
-    })
-    this.isAddingNewTask=false;
-    
-  }
 }
 
